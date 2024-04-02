7d365438b84f0a3a347e3f473d964d569e757966
RestorerBarisereden <restorerzorzor@gmail.com>
2024-04-02 23:51:11 +0100

    first push

---
+++ ass13.js
@@ -0,0 +1,11 @@
+let experience = 5 
+let javascript = false
+if (experience >= 3 && javascript){
+    console.log("Consider for senior developer position");
+}
+else if (experience < 3 && javascript){
+    console.log("Consider for junior developer position");
+}
+else {
+    console.log("Job application denied");
+}
